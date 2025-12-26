8a25c41101bc07284ae02814aedf6b932e168de9
Bibek Bhattarai <bibek.bhattarai072@gmail.com>
2025-12-25 21:50:11 -0500

    pass by value/reference

---
+++ Array/02_PassArray.java
@@ -0,0 +1,32 @@
+//call by reference
+
+class Solution{
+    public static void update(int marks[], int nonChangebale) {
+        nonChangebale = 10;
+        for(int i=0; i<marks.length; i++) {
+            marks[i] =  marks[i] + 1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[] = {97, 98, 99};
+        int nonChangeable = 5;
+        update(marks, nonChangeable); //pass the reference, not the whole array.
+        System.out.println(nonChangeable);
+
+        for(int i=0; i<marks.length; i++) {
+            System.out.println("print: " + marks[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
+
+/*
+output:
+
+5 => nonchangeable value
+print: 98
+print: 99
+print: 100
+
+*/
